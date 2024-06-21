0e0de5571cd6c6728ffc5045dcb3b06808de5ec4
Bluesky428 <bluesky.engineer428@gmail.com>
2024-06-21 14:57:10 -0700

    Add new Features

--- app/components/MarketplaceComponent/firstComponent.tsx
+++ app/components/MarketplaceComponent/firstComponent.tsx
@@ -2,36 +2,77 @@ import { FC } from "react";
 
 const FirstComponent: FC = () => {
     return (
-        <div className="grid md:grid-cols-3 max-w-screen-lg mx-auto gap-10 mt-24">
-
-            <div className="flex gap-4 items-start flex-col ">
-                <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5"><path d="M9.96424 2.68571C10.0668 2.42931 9.94209 2.13833 9.6857 2.03577C9.4293 1.93322 9.13832 2.05792 9.03576 2.31432L5.03576 12.3143C4.9332 12.5707 5.05791 12.8617 5.3143 12.9642C5.5707 13.0668 5.86168 12.9421 5.96424 12.6857L9.96424 2.68571ZM3.85355 5.14646C4.04882 5.34172 4.04882 5.6583 3.85355 5.85356L2.20711 7.50001L3.85355 9.14646C4.04882 9.34172 4.04882 9.6583 3.85355 9.85356C3.65829 10.0488 3.34171 10.0488 3.14645 9.85356L1.14645 7.85356C0.951184 7.6583 0.951184 7.34172 1.14645 7.14646L3.14645 5.14646C3.34171 4.9512 3.65829 4.9512 3.85355 5.14646ZM11.1464 5.14646C11.3417 4.9512 11.6583 4.9512 11.8536 5.14646L13.8536 7.14646C14.0488 7.34172 14.0488 7.6583 13.8536 7.85356L11.8536 9.85356C11.6583 10.0488 11.3417 10.0488 11.1464 9.85356C10.9512 9.6583 10.9512 9.34172 11.1464 9.14646L12.7929 7.50001L11.1464 5.85356C10.9512 5.6583 10.9512 5.34172 11.1464 5.14646Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span>
-                <div>
-                    <h3 className="font-semibold text-xl"> Expressive API</h3>
-                    <p className="mt-1 text-gray-500"> You don't need to be an expert to use our plugin. Our expressive API is
-                        readable and well documented.</p>
+        <section className="container space-y-6 bg-white py-8 mx-auto md:py-12 mt-20">
+            <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4" >
+                <h2 className="font-heading text-2xl leading-[1.1] sm:text-2xl md:text-2xl font-bold text-gray-600">Need something done?</h2>
+                <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">Most viewed and all-time top-selling services</p>
+            </div >
+            <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[77rem] md:grid-cols-4">
+                <div className="relative overflow-hidden rounded-lg bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6"><svg xmlns="http://www.w3.org/2000/svg"
+                        width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+                        strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-terminal">
+                        <polyline points="4 17 10 11 4 5"></polyline>
+                        <line x1="12" y1="19" x2="20" y2="19"></line>
+                    </svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Post a job</h3>
+                            <p className="text-sm text-muted-foreground">An AI-powered prompt builder to help you create stunning images.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6"><svg xmlns="http://www.w3.org/2000/svg"
+                        width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+                        strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-grid">
+                        <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                        <line x1="3" y1="9" x2="21" y2="9"></line>
+                        <line x1="3" y1="15" x2="21" y2="15"></line>
+                        <line x1="9" y1="3" x2="9" y2="21"></line>
+                        <line x1="15" y1="3" x2="15" y2="21"></line>
+                    </svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Choose freelancers</h3>
+                            <p className="text-sm text-muted-foreground">Choose from 16x16, 32x32, 64x64, or 128x128 grids.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6"><svg xmlns="http://www.w3.org/2000/svg"
+                        width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+                        strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sliders-horizontal">
+                        <line x1="21" y1="4" x2="14" y2="4"></line>
+                        <line x1="10" y1="4" x2="3" y2="4"></line>
+                        <line x1="21" y1="12" x2="12" y2="12"></line>
+                        <line x1="8" y1="12" x2="3" y2="12"></line>
+                        <line x1="21" y1="20" x2="16" y2="20"></line>
+                        <line x1="12" y1="20" x2="3" y2="20"></line>
+                        <line x1="14" y1="2" x2="14" y2="6"></line>
+                        <line x1="8" y1="10" x2="8" y2="14"></line>
+                        <line x1="16" y1="18" x2="16" y2="22"></line>
+                    </svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Pay safely</h3>
+                            <p className="text-sm text-muted-foreground">Advanced options for adjusting sampling steps and prompt guidance.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6"><svg xmlns="http://www.w3.org/2000/svg"
+                        width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+                        strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-minus">
+                        <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"></path>
+                        <polyline points="14 2 14 8 20 8"></polyline>
+                        <line x1="9" y1="15" x2="15" y2="15"></line>
+                    </svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">We’re here to help</h3>
+                            <p className="text-sm text-muted-foreground">Isolate the subject of your image by removing the background!</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div className="flex gap-4 items-start flex-col ">
-                <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5"><path d="M0.849976 1.74998C0.849976 1.25292 1.25292 0.849976 1.74998 0.849976H3.24998C3.74703 0.849976 4.14998 1.25292 4.14998 1.74998V2.04998H10.85V1.74998C10.85 1.25292 11.2529 0.849976 11.75 0.849976H13.25C13.747 0.849976 14.15 1.25292 14.15 1.74998V3.24998C14.15 3.74703 13.747 4.14998 13.25 4.14998H12.95V10.85H13.25C13.747 10.85 14.15 11.2529 14.15 11.75V13.25C14.15 13.747 13.747 14.15 13.25 14.15H11.75C11.2529 14.15 10.85 13.747 10.85 13.25V12.95H4.14998V13.25C4.14998 13.747 3.74703 14.15 3.24998 14.15H1.74998C1.25292 14.15 0.849976 13.747 0.849976 13.25V11.75C0.849976 11.2529 1.25292 10.85 1.74998 10.85H2.04998V4.14998H1.74998C1.25292 4.14998 0.849976 3.74703 0.849976 3.24998V1.74998ZM2.94998 4.14998V10.85H3.24998C3.74703 10.85 4.14998 11.2529 4.14998 11.75V12.05H10.85V11.75C10.85 11.2529 11.2529 10.85 11.75 10.85H12.05V4.14998H11.75C11.2529 4.14998 10.85 3.74703 10.85 3.24998V2.94998H4.14998V3.24998C4.14998 3.74703 3.74703 4.14998 3.24998 4.14998H2.94998ZM2.34998 1.74998H1.74998V2.34998V2.64998V3.24998H2.34998H2.64998H3.24998V2.64998V2.34998V1.74998H2.64998H2.34998ZM5.09998 5.99998C5.09998 5.50292 5.50292 5.09998 5.99998 5.09998H6.99998C7.49703 5.09998 7.89998 5.50292 7.89998 5.99998V6.99998C7.89998 7.03591 7.89787 7.07134 7.89378 7.10618C7.92861 7.10208 7.96405 7.09998 7.99998 7.09998H8.99998C9.49703 7.09998 9.89998 7.50292 9.89998 7.99998V8.99998C9.89998 9.49703 9.49703 9.89998 8.99998 9.89998H7.99998C7.50292 9.89998 7.09998 9.49703 7.09998 8.99998V7.99998C7.09998 7.96405 7.10208 7.92861 7.10618 7.89378C7.07134 7.89787 7.03591 7.89998 6.99998 7.89998H5.99998C5.50292 7.89998 5.09998 7.49703 5.09998 6.99998V5.99998ZM6.09998 5.99998H5.99998V6.09998V6.89998V6.99998H6.09998H6.89998H6.99998V6.89998V6.09998V5.99998H6.89998H6.09998ZM7.99998 7.99998H8.09998H8.89998H8.99998V8.09998V8.89998V8.99998H8.89998H8.09998H7.99998V8.89998V8.09998V7.99998ZM2.64998 11.75H2.34998H1.74998V12.35V12.65V13.25H2.34998H2.64998H3.24998V12.65V12.35V11.75H2.64998ZM11.75 1.74998H12.35H12.65H13.25V2.34998V2.64998V3.24998H12.65H12.35H11.75V2.64998V2.34998V1.74998ZM12.65 11.75H12.35H11.75V12.35V12.65V13.25H12.35H12.65H13.25V12.65V12.35V11.75H12.65Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span>
-                <div>
-                    <h3 className="font-semibold text-xl">Highly performant</h3>
-                    <p className="mt-1 text-gray-500"> You can make sure your website or app is highly performant with a built-in
-                        system to help you optimize.</p>
-                </div>
-            </div>
-
-
-            <div className="flex gap-4 items-start flex-col ">
-                <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5"><path d="M7.28856 0.796908C7.42258 0.734364 7.57742 0.734364 7.71144 0.796908L13.7114 3.59691C13.8875 3.67906 14 3.85574 14 4.05V10.95C14 11.1443 13.8875 11.3209 13.7114 11.4031L7.71144 14.2031C7.57742 14.2656 7.42258 14.2656 7.28856 14.2031L1.28856 11.4031C1.11252 11.3209 1 11.1443 1 10.95V4.05C1 3.85574 1.11252 3.67906 1.28856 3.59691L7.28856 0.796908ZM2 4.80578L7 6.93078V12.9649L2 10.6316V4.80578ZM8 12.9649L13 10.6316V4.80578L8 6.93078V12.9649ZM7.5 6.05672L12.2719 4.02866L7.5 1.80176L2.72809 4.02866L7.5 6.05672Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span>
-                <div>
-                    <h3 className="font-semibold text-xl">No dependencies </h3>
-                    <p className="mt-1 text-gray-500"> Our plugins do not have any external dependencies so our plugin has the
-                        minimal footprint possible.</p>
-                </div>
-            </div>
-        </div>
+        </section>
     )
 }
 
