f9cecc91732eff140369f078a2f640046b20ff52
Bluesky428 <bluesky.engineer428@gmail.com>
2024-06-21 15:38:29 -0700

    added new features

--- app/pages/Marketplace/index.tsx
+++ app/pages/Marketplace/index.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import Marketplace from "./marketplace";
 import FirstComponent from "@/app/components/MarketplaceComponent/firstComponent";
+import SecondComponent from "@/app/components/MarketplaceComponent/secondComponent";
 
 const MarketPlaceIndex: FC = () => {
     return (
         <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-white sm:py-12">
             <FirstComponent />
             <Marketplace />
+            <SecondComponent />
         </div>
     )
 }
