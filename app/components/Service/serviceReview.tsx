af6f6ccfa366ad1439955d85f3f2146f353ebdd1
Bluesky428 <bluesky.engineer428@gmail.com>
2024-06-23 20:42:14 -0700

    added new functions

--- app/components/Service/serviceReview.tsx
+++ app/components/Service/serviceReview.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { FC, useState, useEffect } from "react";
+import ServicerReview from "./servicerReview";
 
 interface StarRatingProps {
     totalStars?: number;
@@ -61,10 +62,9 @@ const ServiceReview: FC = () => {
             <div className="flex flex-col lg:w-[65%] md:w-[70%] overflow-hidden rounded-md p-2 sm:p-4">
                 <div className="w-full flex items-center py-2"><h1 className="font-bold text-xl">Review</h1></div>
                 <div className="w-full lg:flex md:flex sm:flex items-center py-2">
-
                     <div className="lg:w-[50%] lg:h-[250px] lg:flex lg:flex-col 
-                                    md:w-[50%] md:h-[250px] 
-                                    sm:w-[50%] sm:h-[200px] 
+                                    md:w-[60%] md:h-[250px] 
+                                    sm:w-[60%] sm:h-[200px] 
                                     w-full h-[200px] justify-between lg:py-4 md:py-4 lg:px-2 md:px-2 sm:px-4">
                         {
                             review.map((item) => (
@@ -79,35 +79,61 @@ const ServiceReview: FC = () => {
                         }
 
                     </div>
-                </div>
-                <div className="w-full flex items-center">
-                    <div className="flex items-center py-2">
-                        <img src="https://demoapus1.com/freeio/wp-content/uploads/2022/10/5-150x150.jpg" className="w-12 h-12 rounded-full" alt="" />
-                    </div>
-                    <div className="flex-flex-col justify-center py-2 px-4">
-                        <div className="text-sm">Admin</div>
-                        <div className="tex-sm flex items-center">
-                            <svg
-                                className="w-3 h-3 text-yellow-300"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 22 20"
-                            >
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                            <span className="px-2">3.0</span>
-                            <div>November 9, 2022</div>
+                    <div className="lg:w-[50%] lg:h-[250px] lg:flex lg:flex-col 
+                                    md:w-[40%] md:h-[250px] 
+                                    sm:w-[40%] sm:h-[200px] 
+                                    w-full h-[200px] justify-between lg:py-4 md:py-4 lg:px-2 md:px-2 sm:px-4">
+                        <div className="w-full flex items-center mt-4 py-2">
+                            <div className="lg:w-full text-sm font-bold text-black-500">Rating Breakdown</div>
+                        </div>
+                        <div className="lg:w-full text-sm font-bold text-black-500 flex justify-between py-2">
+                            <div className="w-auto flex items-center">Seller Communication level</div>
+                            <div className="w-auto flex items-center">
+                                <svg
+                                    className="w-5 h-5 text-yellow-300 ms-1"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 22 20"
+                                >
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <span className="px-2">4.8</span>
+                            </div>
+                        </div>
+                        <div className="lg:w-full text-sm font-bold text-black-500 flex justify-between py-2">
+                            <div>Recommend to a friend</div>
+                            <div className="w-auto flex items-center">
+                                <svg
+                                    className="w-5 h-5 text-yellow-300 ms-1"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 22 20"
+                                >
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <span className="px-2">4.8</span>
+                            </div>
+                        </div>
+                        <div className="lg:w-full text-sm font-bold text-black-500 flex justify-between py-2">
+                            <div>Services as described</div>
+                            <div className="w-auto flex items-center">
+                                <svg
+                                    className="w-5 h-5 text-yellow-300 ms-1"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 22 20"
+                                >
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <span className="px-2">4.8</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="w-full flex items-center">
-                    <span className="text-sm">
-                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur
-                        aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi
-                        nesciunt. Neque porro quisquam est.
-                    </span>
-                </div>
+                <ServicerReview />
                 <div className={`lg:w-full h-auto py-4 lg:flex`} style={{ "display": isMobileMode ? "" : "none" }}>
                     <div className="flex flex-col w-full p-6 px-2 text-center text-gray-900 bg-white border shadow-lg
                                 border-[#581C87] rounded-lg shadow dark:border-[#581C87] xl:p-8 dark:bg-white dark:text-white">
---
+++ app/components/Service/servicerReview.tsx
@@ -0,0 +1,58 @@
+import { FC } from "react";
+
+const ServicerReview: FC = () => {
+    return (
+        <div className="flex items-start">
+            <div className="flex-shrink-0">
+                <div className="inline-block relative">
+                    <div className="relative w-16 h-16 rounded-full overflow-hidden">
+                        <img className="absolute top-0 left-0 w-full h-full bg-cover object-fit object-cover" src="https://demoapus1.com/freeio/wp-content/uploads/2022/10/5-150x150.jpg" alt="Profile picture" />
+                        <div className="absolute top-0 left-0 w-full h-full rounded-full shadow-inner"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="ml-6">
+                <p className="flex items-baseline">
+                    <span className="text-gray-600 font-bold">John Powell</span>
+                </p>
+                <p className="flex items-baseline">
+                    <span className="text-gray-600 text-sm mt-2">Los Angels</span>
+                </p>
+                <div className="flex items-center mt-4 text-gray-600">
+                    <div className="flex items-center">
+                        <div className="flex items-center">
+                            <svg className="w-3 h-3 fill-current text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
+                            <svg className="w-3 h-3 fill-current text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
+                            <svg className="w-3 h-3 fill-current text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
+                            <svg className="w-3 h-3 fill-current text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
+                            <svg className="w-3 h-3 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
+                        </div>
+                        <span className="text-sm px-2">1 month ago</span>
+                    </div>
+                </div>
+                <div className="mt-3">
+                    <span className="font-bold">Sapien consequat eleifend!</span>
+                    <p className="mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+                <div className="flex items-center justify-between mt-4 text-sm text-gray-600 fill-current">
+                    <div className="flex items-center">
+                        <span>Helplful?</span>
+                        <button className="flex items-center ml-6">
+                            <svg className="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M11 0h1v3l3 7v8a2 2 0 0 1-2 2H5c-1.1 0-2.31-.84-2.7-1.88L0 12v-2a2 2 0 0 1 2-2h7V2a2 2 0 0 1 2-2zm6 10h3v10h-3V10z" /></svg>
+                            <span className="ml-2">56</span>
+                        </button>
+                        <button className="flex items-center ml-4">
+                            <svg className="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M11 20a2 2 0 0 1-2-2v-6H2a2 2 0 0 1-2-2V8l2.3-6.12A3.11 3.11 0 0 1 5 0h8a2 2 0 0 1 2 2v8l-3 7v3h-1zm6-10V0h3v10h-3z" /></svg>
+                            <span className="ml-2">10</span>
+                        </button>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between mt-4 text-sm text-gray-600 fill-current">
+                    <button type="button" className="w-36 py-3 border border-[#581C87] text-[#681C87] hover:bg-[#581C87] hover:text-white transition duration-300 rounded-sm">Show more</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ServicerReview;
